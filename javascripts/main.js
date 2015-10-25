91805d0c72adf4433e33ab956a750585c4d59f67
Joshua Porter <joshporter1@gmail.com>
2015-10-25 19:41:33 -0400

    minor style changes. added smooth scrolling

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,10 @@
-console.log('This would be the main JS file.');
-
+$(function(){
+  // smooth scrolling on link click
+	$('a[href^=#]').on('click', function(){
+    console.log(this);
+    $('html, body').animate({
+      scrollTop: $($.attr(this,'href')).offset().top
+    }, 500);
+    return false;
+	});
+});
